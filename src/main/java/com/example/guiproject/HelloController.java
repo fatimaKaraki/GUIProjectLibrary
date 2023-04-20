3f005f1ee9ade2e3d3905157df7e0c1ec8e46196
Fadel <fadel.bayloun12@gmail.com>
2023-04-21 02:47:19 +0300

    here I did the Login page design

--- src/main/java/com/example/guiproject/HelloController.java
+++ src/main/java/com/example/guiproject/HelloController.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
