0c06ad53435cf18853cbfff3295e05a4a23adf08
IssaAlSabeh <issasab2204@icloud.com>
2023-04-20 12:23:55 +0300

    Testing

--- src/main/java/com/example/guiproject/HelloApplication.java
+++ src/main/java/com/example/guiproject/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    // test text to test the push
     public static void main(String[] args) {
         launch();
     }
