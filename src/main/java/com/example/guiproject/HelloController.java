48e21d6eabad6c958ca11610096851cc7ae18086
Fadel <fadel.bayloun12@gmail.com>
2023-04-20 14:00:51 +0300

    The login and the start of the controller

--- src/main/java/com/example/guiproject/HelloController.java
+++ src/main/java/com/example/guiproject/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.guiproject;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -10,5 +14,21 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public static class HelloApplication extends Application {
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("library");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+
+        public static void main(String[] args) {
+            launch();
+        }
     }
 }
--- src/main/java/com/example/guiproject/controllers/loginController.java
+++ src/main/java/com/example/guiproject/controllers/loginController.java
@@ -1,4 +1,32 @@
 package com.example.guiproject.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class loginController {
+
+    @FXML
+    private Button login;
+    @FXML
+    private Button clear;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    public void login(ActionEvent event) throws IOException {
+        String name = username.getText();
+        String pass = password.getText();
+
+
+    }
+
+    public void clear(){
+
+    }
 }
