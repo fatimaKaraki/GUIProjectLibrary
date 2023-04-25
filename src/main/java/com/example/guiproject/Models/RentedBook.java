7e57ba6127f4cc497aa4bcbde2e540e95762457b
IssaAlSabeh <issasab2204@icloud.com>
2023-04-25 20:55:44 +0300

    We created the rented book DAO and model and we just need to test them, we also updated the home page to show the rented books

--- src/main/java/com/example/guiproject/Models/RentedBook.java
+++ src/main/java/com/example/guiproject/Models/RentedBook.java
@@ -1,2 +1,40 @@
-package com.example.guiproject.Models;public class RentedBook {
+package com.example.guiproject.Models;
+
+import java.util.Date;
+
+public class RentedBook {
+    private int bookId;
+    private int memberId;
+    private Date returnDate;
+
+
+    public RentedBook(int bookId, int memberId, Date returnDate) {
+        this.bookId = bookId;
+        this.memberId = memberId;
+        this.returnDate = returnDate;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
 }
--- src/main/java/com/example/guiproject/controllers/homeController.java
+++ src/main/java/com/example/guiproject/controllers/homeController.java
@@ -1,4 +1,41 @@
 package com.example.guiproject.controllers;
 
-public class homeController {
+import com.example.guiproject.DAO.RentedBookDAO;
+import com.example.guiproject.Models.RentedBook;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date; // or import java.sql.Date;
+import java.util.ResourceBundle;
+
+public class homeController implements Initializable {
+
+    @FXML
+    public TableView rentedBooksTable;
+    @FXML
+    public TableColumn<RentedBook,Integer> rentedBookColumn;
+    @FXML
+    public TableColumn<RentedBook,Integer> rentedMemberColumn;
+    @FXML
+    public TableColumn<RentedBook, Date> rentedDateColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            RentedBookDAO rentedBookDAO = new RentedBookDAO();
+            ObservableList<RentedBook> rentedBooks = (ObservableList<RentedBook>) rentedBookDAO.getAllRentedBooks();
+            //casting may cause some errors
+            // pay attention to this
+            rentedBooksTable.setItems(rentedBooks);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
